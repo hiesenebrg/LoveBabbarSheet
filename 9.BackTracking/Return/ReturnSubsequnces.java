615b042f6944068239bf16499bae1e64cc519742
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-25 10:19:02 +0530

    return and print folder created in backtracking folder

---
+++ 9.BackTracking/Return/ReturnSubsequnces.java
@@ -0,0 +1,30 @@
+package Return;
+import java.io.ObjectInputFilter.FilterInfo;
+import java.util.Scanner;
+
+public class ReturnSubsequnces {
+    public static String[] Returnsubsequnces(String string){
+        if(string.length()==0){
+            String[] ans = new String[1];
+            ans[0] = "";
+            return ans;
+        }
+        String [] output = Returnsubsequnces(string.substring(1));
+        String[] ans = new String[output.length*2];
+        for (int i = 0; i < output.length; i++) {
+            ans[i] = output[i];
+        }
+        for (int i = 0; i < output.length; i++) {
+            ans[output.length+i] = string.charAt(0) + output[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        String[] ans = Returnsubsequnces(string);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
