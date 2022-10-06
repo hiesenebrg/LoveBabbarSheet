596ecab3c63c6d965d02f15c5494bc827794846b
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-06 16:35:07 +0530

    DP - minimum steps to one

---
+++ 14.Dynamic Programming/MinStepsToOne.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+import javax.xml.transform.Source;
+public class MinStepsToOne {
+    public static int MinStepsToOne(int n) {
+        if (n == 1) {
+			return 0;
+		}
+
+		int[] minSteps = new int[n + 1];
+		
+		minSteps[1] = 0;
+
+	    for (int currStep = 2; currStep <= n; currStep++) {
+
+			int subtractOne = Integer.MAX_VALUE;
+		    int divideByTwo = Integer.MAX_VALUE;
+		    int divideByThree = Integer.MAX_VALUE;
+
+	    	subtractOne = minSteps[currStep - 1];
+	    	
+	    	if (currStep % 2 == 0) {
+	    		divideByTwo = minSteps[currStep / 2];
+	    	}
+
+	    	if (currStep % 3 == 0) {
+	    		divideByThree = minSteps[currStep / 3];
+	    	}
+
+	    	minSteps[currStep] = 1 + Math.min(subtractOne, Math.min(divideByTwo, divideByThree));
+	    }
+
+	    return minSteps[n];
+	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(MinStepsToOne(n));
+    }
+
+
+    }
+
