3fa93ddf06bab3e9c23a34e389da161446d1f445
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-11 07:30:24 +0530

    DP - Longest Common Subsequnces

--- 14.Dynamic Programming/LCS.java
+++ 14.Dynamic Programming/LCS.java
@@ -1,27 +1,32 @@
 
-
 public class LCS {
-    public static int  lcs(String s1 , String s2) {
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
-        for (int i = s1.length(); i >=0 ; i--) {
-            for (int j = s2.length(); j >=0; j--) {
-                if(i==s1.length() || j==s2.length()){
-                    dp[i][j] = 0;               
-                 }else if(s1.charAt(i) == (s2.charAt(j))){
-                    dp[i][j] = 1+ dp[i+1][j+1];
-                 }
-                 else{
-                    dp[i][j] = Math.max(dp[i+1][j] , dp[i][j+1]);
-                 }
+    public static int lcs( String s1, String s2) {
+        int x =s1.length() , y = s2.length();
+        int L[][] = new int[x + 1][y + 1];
+
+        /*
+         * Following steps build L[m+1][n+1] in bottom up fashion. Note
+         * that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]
+         */
+
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    L[i][j] = L[i - 1][j - 1] + 1;
+                else
+                    L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
             }
         }
-        
-        return dp[0][0]; 
+        return L[x][y];
     }
+
+
+
     public static void main(String[] args) {
         String s1 = "adebc";
         String s2 = "dcadb";
         System.out.println(lcs(s1, s2));
     }
-    
+
 }
