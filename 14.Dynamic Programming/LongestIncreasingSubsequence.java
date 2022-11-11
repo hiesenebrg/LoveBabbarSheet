b2e51521239b2f785240beb5f85151e7ecf11fdf
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-11 08:43:12 +0530

    DP - Longest Increasing Subsequence

---
+++ 14.Dynamic Programming/LongestIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+public class LongestIncreasingSubsequence {
+    public static int longestSubsequence(int size, int a[])
+    {
+     int[] dp = new int[size];
+     dp[0]=1;
+     int overall_max = 0;
+     for(int i =1; i<dp.length;i++){
+         int max = 0;
+         for(int j = 0;j<i;j++){
+             if(a[j]<a[i]){
+                 if(dp[j]>max){
+                     max = dp[j];
+                 }
+             }
+         }
+         dp[i] = max+1;
+         if(dp[i]>overall_max){
+             overall_max = dp[i];
+         }
+     }
+     
+     return overall_max;
+    }
+}
