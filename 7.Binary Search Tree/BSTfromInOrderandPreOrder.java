60a3d7c3a1bef6f7c315afbe2ea4917900061d81
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-16 12:15:51 +0530

    Construct Tree from Inorder & Preorder

---
+++ 7.Binary Search Tree/BSTfromInOrderandPreOrder.java
@@ -0,0 +1,43 @@
+public class BSTfromInOrderandPreOrder {
+    public static BinaryTreeNode<Integer> buildTree(int[] preOrder, int[] inOrder) {
+        // Your code goes here
+        BinaryTreeNode<Integer> root = buildTreeFromPreInHelper(preOrder, inOrder, 0, preOrder.length - 1, 0,
+                inOrder.length - 1);
+        return root;
+    }
+
+    public static BinaryTreeNode<Integer> buildTreeFromPreInHelper(int pre[], int in[], int siPre, int eiPre, int siIn,
+            int eiIn) {
+        if (siPre > eiPre) {
+            return null;
+        }
+        int rootData = pre[siPre];
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+
+        int rootIndex = -1;
+        for (int i = siIn; i <= eiIn; i++) {
+            if (in[i] == rootData) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+        int siPreLeft = siPre + 1;
+        int siInLeft = siIn;
+        int eiInLeft = rootIndex - 1;
+        int siInRight = rootIndex + 1;
+
+        int eiPreRight = eiPre;
+
+        int leftSubTreeLength = eiInLeft - siInLeft + 1;
+
+        int eiPreLeft = siPreLeft + leftSubTreeLength - 1;
+        int siPreRight = eiPreLeft + 1;
+
+        BinaryTreeNode<Integer> left = buildTreeFromPreInHelper(pre, in, siPreLeft, eiPreLeft, siInLeft, eiInLeft);
+        BinaryTreeNode<Integer> right = buildTreeFromPreInHelper(pre, in, siPreRight, eiPreRight, siInRight, eiInRight);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
