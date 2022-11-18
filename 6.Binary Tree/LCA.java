395805faee7689f6b9c801bbf1926af55d56fee3
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-18 09:31:35 +0530

    Lowest Common Ancestor

---
+++ 6.Binary Tree/LCA.java
@@ -0,0 +1,17 @@
+public class LCA {
+    Node lca(Node root, int n1, int n2) {
+        if (root == null)
+            return null;
+        if (root.data == n1 || root.data == n2)
+            return root;
+        Node left = lca(root.left, n1, n2);
+        Node right = lca(root.right, n1, n2);
+        if (left != null && right != null)
+            return root;
+        if (left != null && right == null)
+            return left;
+        else
+            return right;
+
+    }
+}
