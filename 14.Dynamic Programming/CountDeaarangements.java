36d3abcc69151eba0343815951c837109a7caceb
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-18 08:59:23 +0530

    DP - Count Derangements (Permutation such that no element appears in its original position)

---
+++ 14.Dynamic Programming/CountDeaarangements.java
@@ -0,0 +1,35 @@
+/**
+ * CountDeaarangements
+ */
+public class CountDeaarangements {
+
+    public static int countDer(int n)
+    {
+        // Create an array to store
+        // counts for subproblems
+        int der[] = new int[n + 1];
+     
+        // Base cases
+        der[1] = 0;
+        der[2] = 1;
+     
+        // Fill der[0..n] in bottom up
+        // manner using above recursive
+        // formula
+        for (int i = 3; i <= n; ++i)
+            der[i] = (i - 1) * (der[i - 1] +
+                                der[i - 2]);
+     
+        // Return result for n
+        return der[n];
+    }
+     
+    // Driver program
+    public static void main (String[] args)
+    {
+        int n = 4;
+        System.out.println("Count of Derangements is " +
+                            countDer(n));
+     
+    }
+}
