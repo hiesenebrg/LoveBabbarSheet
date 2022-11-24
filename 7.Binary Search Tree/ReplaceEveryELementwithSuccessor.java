14d18deee970b97d8855d73fe4e3fc8d3d47ee94
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-24 09:30:44 +0530

    BST - Replace every element with the least greater element on its right

---
+++ 7.Binary Search Tree/ReplaceEveryELementwithSuccessor.java
@@ -0,0 +1,25 @@
+public class ReplaceEveryELementwithSuccessor {
+    Node insert(Node root, int data, Node succ) {
+        if (root == null)
+            return root = new Node();
+        if (data < root.data) {
+            succ = root.data;
+            insert(root.left, data, succ);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data, succ);
+        }
+        return root;
+    }
+
+    void replace(int[] arr, int n) {
+        Node root = bull;
+        for (int i = n - 1; i >= 0; i--) {
+            Node succ = null;
+            succ = insert(root, arr[i], succ);
+            if (succ == null)
+                arr[i] = -1;
+            else
+                arr[i] = succ.data;
+        }
+    }
+}
