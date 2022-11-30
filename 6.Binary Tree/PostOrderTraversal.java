44e77f8a5dbead757e858094215ea044cd4dad89
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-30 11:16:15 +0530

    Binary Tree - PostOrder Traversal

---
+++ 6.Binary Tree/PostOrderTraversal.java
@@ -0,0 +1,13 @@
+public class PostOrderTraversal {
+    public static void postOrder(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+        if(root == null){
+            return;
+        }
+         
+         postOrder(root.left);
+       
+         postOrder(root.right);
+         System.out.print(root.data+ " ");
+	}
+}
