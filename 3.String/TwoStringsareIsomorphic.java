107f1916679c1eede85e8cc7848f2fc02e9c6c5a
hiesenebrg <adarshsingh8008@gmail.com>
2022-12-16 11:01:56 +0530

    Strings - Isomorphic Strings

---
+++ 3.String/TwoStringsareIsomorphic.java
@@ -0,0 +1,28 @@
+public class TwoStringsareIsomorphic {
+    // the answer is in python , please convert into java
+    // def areIsomorphic(self,str1,str2):
+    //     d = {}
+    //     e = {}
+    //     if(len(str1)!=len(str2)):
+    //         return 0
+        
+    //     for i in range(len(str1)):
+    //         if str1[i] not in d and str2[i] not in e:
+    //             d[str1[i]] = str2[i]
+    //             e[str2[i]] = str1[i]
+    //         else:
+    //             if(str2[i] in e):
+    //                 if( e[str2[i]] != str1[i]):
+    //                     return 0
+                
+    //             if(str1[i] in d):
+    //                 if(d[str1[i]] != str2[i]):
+    //                     return 0
+                    
+                    
+            
+            
+                
+            
+    //     return 1
+}
